8d9edeb53f3863b7d91983ae02b033128173d953
Richa-12y <kricha000@gmail.com>
2021-12-06 20:57:00 +0530

    React project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import Cube from 'react-cube-3d-animation'
+import React, { useEffect } from 'react';
 
-function App() {
+
+function App ()
+{
+  const [data, setData] = React.useState([])
+  React.useEffect(() =>
+  {
+
+    const ApiLink = 'https://fakestoreapi.com/products/'
+    const dataFetching = async () =>
+    {
+      const data = await fetch(ApiLink)
+
+      const jdtaa = await data.json()
+      setData(jdtaa)
+
+    }
+    dataFetching()
+  }, [])
+  console.log(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>RMart</h1>
+      <p className="degp">Explore hera for more product</p>
+      <Cube top_Color= '#f30e58' box_plate_linear_gradient_first_color='#6163a5' box_plate_linear_gradient_Second_color='#0acfcd' box_Shadow_Color='rgb(37,209,29)' size='5' />
+            <div>
+        {
+          data.map((e) =>
+          {
+            return (<>
+              <div className="imgd">
+              <div className="imgbackg" style={{backgroundImage:`url(${e.image})`
+            }}>
+              </div>
+              </div>
+              <div className="titledeg">
+              
+              <h2>{e.title}</h2>
+              </div>
+              <div className="pdrdegin">
+                <p>{e.price}</p>
+                <p>{e.description}</p>
+                <p>{e.rating.count}</p>
+                <p>{e.rating.rate}</p>
+                <button>Buy Product</button>
+              </div>
+
+
+            </>)
+          })
+
+        }
+
+
+      </div>
+
+    </>
   );
 }
 
